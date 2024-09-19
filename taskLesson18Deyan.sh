c50e06cd7c80097e7d27261d9128d7bb46e21554
DeyanSpasovBerlin <pislpe@gmail.com>
2024-09-19 22:28:05 +0200

    Create taskLesson18Deyan.sh

---
+++ taskLesson18Deyan.sh
@@ -0,0 +1,13 @@
+-rwxrw-r--  1 ec2-user ec2-user    70 Sep  6 12:03 Lessom15.sh
+-rw-rw-r--  1 ec2-user ec2-user    40 Sep  5 18:19 <Lesson14.sh>
+-rwxrw-r--  1 ec2-user ec2-user   142 Sep  6 11:21 Lesson14.sh
+-rw-rw-r--  1 ec2-user ec2-user     0 Sep  5 19:43 Lesson14.txt
+-rwxrw-r--  1 ec2-user ec2-user   313 Sep 13 13:24 Lesson17.sh
+-rwxrw-r--  1 ec2-user ec2-user   161 Aug 29 20:29 myfirstbashscript.sh
+-rw-rw-r--  1 ec2-user ec2-user  1640 Sep 13 13:28 output.txt
+-rw-rw-r--  1 ec2-user ec2-user 22204 Sep  5 11:53 processes.txt
+-rwxrw-r--  1 ec2-user ec2-user   437 Sep 19 20:06 taskLesson18Deyan.sh
+drwxrwxr-x  2 ec2-user ec2-user   297 Sep 19 19:55 test_dir
+[ec2-user@ip-172-31-33-20 DeyanSpasov]$
+
+
